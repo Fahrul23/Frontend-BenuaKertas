9d7ff29946851dc5e92fe4d97c82268e93ae20bd
fahrul ihsan <ihsanfahrul23@gmail.com>
2026-07-22 22:56:51 +0700

    fix: url APIBASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import api from './axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/v1';
 /**
  * API Service for Master Data
  */
